8602fb9ff1b29094c373ca8e5570dd7b2f3680b5
Shubhithebeast <Shubhithebeast@gmail.com>
2024-02-21 11:14:26 +0530

    update

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+
+const express = require('express')
+const app = express()
+const port = 5000
+const mongodb=require("./db");
+const cors = require('cors');
+
+ 
+mongodb();
+
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use('/api/auth', require('./Routes/Auth'));
+
+app.listen(port,()=>{
+  console.log(`Server running on ${port}`);
+}); 
+
